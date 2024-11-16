61c8e292e81676c4c4d87b5021bcbcc86bbd704b
Jetmir <jetmirdemiri@91.life>
2024-11-16 12:29:01 +0100

    Changed the priority cards design

--- app/DashboardPage/priorityCards.tsx
+++ app/DashboardPage/priorityCards.tsx
@@ -6,8 +6,12 @@ import { lineChart } from "@/components/forms/lineChart";
 import { horizontalBarChart } from "@/components/forms/horizontalBarChart";
 import PieChartComponent from "../../components/forms/pieChart";
 
+const filterBySentiment = (employees: any[], sentiment: string) => {
+    return employees.filter(employee => employee.sentiment === sentiment);
+}
+
 interface Employee {
-    id?: string; 
+    id?: string;
     firstName: string;
     lastName: string;
     riskScore: number;
@@ -53,55 +57,142 @@ function MyAreaChart({ employees }: { employees: Employee[] }) {
                     className={styles.searchInput}
                 />
             </div>
-            
+
+
+
             {/* Cards Section */}
             <div className={`${styles.priorityCards} ${visibleCardsCount === 1 ? styles.centeredCard : ''}`}>
                 {showLowRisk && (
-                    <div className={`${styles.card} ${styles.lowRisk}`}>
-                        <h1>Low Risk</h1>
-                        <ul className={styles.list}>
-                            {lowRiskEmployees.map((employee, index) => (
-                                <li 
-                                    key={employee.id || `${employee.firstName}-${employee.lastName}-low-${index}`} 
-                                    className={styles.listItem}
-                                >
-                                    <span>{`${employee.firstName} ${employee.lastName}`}</span>
-                                    <span>({employee.riskScore})</span>
-                                </li>
-                            ))}
-                        </ul>
+                    <div className={`${styles.card} ${styles.positive}`} style={{ position: 'relative', padding: '20px' }}>
+                        <h1 style={{ fontSize: '14px', fontWeight: 500, marginTop: '-3px' }}>Low Risk</h1>
+
+                        <div style={{
+                            position: 'absolute',
+                            left: 0,
+                            right: 0,
+                            height: '19px',
+                            backgroundColor: '#F7F8FB',
+                            top: '50px',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            padding: '0 20px',
+                            fontWeight: 600,
+                            fontSize: '12px',
+                            color: '#B7BCCA',
+                            marginTop: '3px',
+                        }}>
+                            <p style={{ margin: 0 }}>Name</p>
+                            <p style={{ margin: 0 }}>Score</p>
+                        </div>
+
+                        <div
+                            style={{ marginTop: '40px', maxHeight: '110px', overflowY: 'auto', zIndex: 1 }}
+                            className={`${styles.scrollContainer}`}
+                        >
+                            <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+                                {lowRiskEmployees.map((employee, index) => (
+                                    <li key={employee.id} style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        fontSize: '13px',
+                                        padding: '5px ',
+                                        borderBottom: '1px solid #F1F2F6',
+                                        ...(index === lowRiskEmployees.length - 1 && { borderBottom: 'none' })
+                                    }}>
+                                        <span>{`${employee.firstName} ${employee.lastName}`}</span>
+                                        <span>{(employee.riskScore).toFixed(0)}%</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
                     </div>
+
                 )}
                 {showMediumRisk && (
-                    <div className={`${styles.card} ${styles.mediumRisk}`}>
-                        <h1>Medium Risk</h1>
-                        <ul className={styles.list}>
-                            {mediumRiskEmployees.map((employee, index) => (
-                                <li 
-                                    key={employee.id || `${employee.firstName}-${employee.lastName}-medium-${index}`} 
-                                    className={styles.listItem}
-                                >
-                                    <span>{`${employee.firstName} ${employee.lastName}`}</span>
-                                    <span>({employee.riskScore})</span>
-                                </li>
-                            ))}
-                        </ul>
+                    <div className={`${styles.card} ${styles.neutral}`} style={{ position: 'relative', padding: '20px' }}>
+                        <h1 style={{ fontSize: '14px', fontWeight: 500, marginTop: '-3px' }}>Medium Risk</h1>
+
+                        <div style={{
+                            position: 'absolute',
+                            left: 0,
+                            right: 0,
+                            height: '19px',
+                            backgroundColor: '#F7F8FB',
+                            top: '50px',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            padding: '0 20px',
+                            fontWeight: 600,
+                            fontSize: '12px',
+                            color: '#B7BCCA',
+                            marginTop: '3px'
+                        }}>
+                            <p style={{ margin: 0 }}>Name</p>
+                            <p style={{ margin: 0 }}>Score</p>
+                        </div>
+
+                        <div style={{ paddingTop: '40px' }}>
+                            <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+                                {mediumRiskEmployees.map((employee, index) => (
+                                    <li key={employee.id} style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        fontSize: '13px',
+                                        padding: '5px ',
+                                        borderBottom: '1px solid #F1F2F6',
+                                        ...(index === mediumRiskEmployees.length - 1 && { borderBottom: 'none' })
+                                    }}>
+                                        <span>{`${employee.firstName} ${employee.lastName}`}</span>
+                                        <span>{(employee.riskScore).toFixed(0)}%</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
                     </div>
                 )}
                 {showHighRisk && (
-                    <div className={`${styles.card} ${styles.highRisk}`}>
-                        <h1>High Risk</h1>
-                        <ul className={styles.list}>
-                            {highRiskEmployees.map((employee, index) => (
-                                <li 
-                                    key={employee.id || `${employee.firstName}-${employee.lastName}-high-${index}`} 
-                                    className={styles.listItem}
-                                >
-                                    <span>{`${employee.firstName} ${employee.lastName}`}</span>
-                                    <span>({employee.riskScore})</span>
-                                </li>
-                            ))}
-                        </ul>
+                    <div className={`${styles.card} ${styles.negative}`} style={{ position: 'relative', padding: '20px' }}>
+                        <h1 style={{ fontSize: '14px', fontWeight: 500, marginTop: '-3px' }}>High Risk</h1>
+
+                        <div style={{
+                            position: 'absolute',
+                            left: 0,
+                            right: 0,
+                            height: '19px',
+                            backgroundColor: '#F7F8FB',
+                            top: '50px',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            padding: '0 20px',
+                            fontWeight: 600,
+                            fontSize: '12px',
+                            color: '#B7BCCA',
+                            marginTop: '3px'
+                        }}>
+                            <p style={{ margin: 0 }}>Name</p>
+                            <p style={{ margin: 0 }}>Score</p>
+                        </div>
+
+                        <div style={{ paddingTop: '40px' }}>
+                            <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+                                {highRiskEmployees.map((employee, index) => (
+                                    <li key={employee.id} style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        fontSize: '13px',
+                                        padding: '5px ',
+                                        borderBottom: '1px solid #F1F2F6',
+                                        ...(index === highRiskEmployees.length - 1 && { borderBottom: 'none' })
+                                    }}>
+                                        <span>{`${employee.firstName} ${employee.lastName}`}</span>
+                                        <span>{(employee.riskScore).toFixed(0)}%</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
                     </div>
                 )}
             </div>
