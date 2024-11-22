b2ce8e311b61a58a9bf697fec7a81b6dd1ec5a30
Jetmir <jetmirdemiri@91.life>
2024-11-22 10:50:13 +0100

    Changed website logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <img src='/LOGOTELLHR.png' style={{width:'11%'}}/>
+                    <img src='/LOGOTELLHR.png' style={{width:'60%', marginLeft:'21%'}}/>
                     {/* <div className="flex items-center gap-2">
                       <DeployButton />
                     </div> */}
